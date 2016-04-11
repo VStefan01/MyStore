9013923b97c8dbe4aca93943a4595cbfec7aaafb
Stefan <vstefan01@gmail.com>
2016-04-11 23:56:51 +0300

    Prima Pagina

---
+++ MyStore/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using MyStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        private StoreDatabaseContext _db = new StoreDatabaseContext();
+
+        // GET: Categories
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.Categories.ToListAsync());
+        }
+    }
+}
---
+++ MyStore/Data/StoreDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using MyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyStore.Data
+{
+    public class StoreDatabaseContext : DbContext
+    {
+        public StoreDatabaseContext() : base("Server=(localdb)\\ProjectsV12;AttachDbFilename=H:\\D\\Curs .Net 2015\\MyProjects\\ProiectFinal\\MyStore.mdf; Integrated Security=Yes;")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
